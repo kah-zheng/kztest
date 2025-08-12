efee9a24a403ab806c6905a7403523933970cd4a
Kz <167838135+kah-zheng@users.noreply.github.com>
2025-08-12 16:20:37 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def root():
-    return "Hello from CI/CD pipeline! trying changes"
+    return "Hello from CI/CD pipeline! trying changes 2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
